ea2dcb1e6099405d4b78876dec8c43eb67403f00
Lukas Simko <sompo74@gmail.com>
2017-10-27 20:22:55 +0100

    Get Patient by ID

--- test/routes/patients-test.js
+++ test/routes/patients-test.js
@@ -19,7 +19,18 @@ describe('patients', function (){
                 });
         });
     });
-
+    describe('GET /patients', function () {
+        it('should return the Patients in Collection', function(done) {
+            chai.request(server)
+                .get('/patients/59fb7aed01cb542e54ddc330')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array');
+                    expect(res.body.length).to.equal(1);
+                    done();
+                });
+        });
+    });
 
 
 });
