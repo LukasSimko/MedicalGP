38c32a34154a84a5ad7e70d589bf3aef0dfb3ebd
Lukas Simko <sompo74@gmail.com>
2017-10-31 18:46:24 +0000

    Patients

---
+++ models/patients.js
@@ -0,0 +1,8 @@
+var patients =[
+    {id:0000,first:"Lukas",last:"Simko",age:31,gender:"male",mobile:0876278131,visit:0},
+    {id:0000,first:"Peter",last:"Pan",age:31,gender:"male",mobile:0876678831,visit:0},
+    {id:0000,first:"Connor",last:"Mc",age:31,gender:"male",mobile:0866558021,visit:0},
+    {id:0000,first:"Jen",last:"Quick",age:31,gender:"male",mobile:0856670001,visit:0},
+];
+
+module.exports = patients;
