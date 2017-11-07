286f329b4a39d1ace983be62678ec347567c1bf7
Lukas Simko <sompo74@gmail.com>
2017-11-07 14:54:17 +0000

    Update doctor cost

--- test/routes/doctors-test.js
+++ test/routes/doctors-test.js
@@ -1,6 +1,8 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../../bin/www');
+var Doctor = require ('../../models/doctors');
+var mongoose = require('mongoose');
 var expect = chai.expect;
 
 chai.use(chaiHttp);
@@ -19,6 +21,49 @@ db.once('open', function () {
 
 
 describe('doctor', function (){
+    beforeEach(function (done) {
+
+        Doctor.remove({}, function (err) {
+
+            if (err)
+                done(err);
+            else {
+                var doctor1 = new Doctor();
+
+                doctor1._id = "59f6f0b99bd9dc7f555a7dac";
+                doctor1.fname = "Renata";
+                doctor1.sname = "Simkova";
+                doctor1.location = "Clonmel";
+                doctor1.contact = 353625178131;
+                doctor1.cost = 50;
+
+
+
+                doctor1.save(function (err) {
+                    if (err)
+                        console.log(err);
+                    else {
+                        var doctor2 = new Doctor();
+
+                        doctor2._id = "59f6f0b99bd0000f555a7dac";
+                        doctor2.fname = "Peter";
+                        doctor2.sname = "Korfanta";
+                        doctor2.location = "Cork";
+                        doctor2.contact = 353625178131;
+                        doctor2.cost = 50;
+
+                        doctor2.save(function (err) {
+                            if (err)
+                                console.log(err);
+                            else {
+                                done();
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
     describe('GET /doctor', function () {
         it('should return all the Doctors in Collection', function(done) {
             chai.request(server)
@@ -26,7 +71,7 @@ describe('doctor', function (){
                 .end(function(err, res) {
                     expect(res).to.have.status(200);
                     expect(res.body).to.be.a('array');
-                    expect(res.body.length).to.equal(6);
+                    expect(res.body.length).to.equal(2);
                     done();
                 });
         });
@@ -65,19 +110,33 @@ describe('doctor', function (){
         });
     });
 
+    describe('PUT /doctor/:id/cost', function () {
+        it('should return all Doctors with specified doctor update visit by 1', function(done) {
+            chai.request(server)
+                .put('/doctor/59f6f0b99bd9dc7f555a7dac/cost')
+                .end(function(err, res) {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('message').equal('Price per visit updated!');
+                    done();
+                });
+        });
+
+
+    });
+
     describe(' /DELETE/doctor/:id', function ()  {
-        it('should delete a object from patients database with given id', function(done) {
+        it('should delete a object from doctor database with given id', function(done) {
             beforeEach(function(){
                 while(doctor.length > 0) {
                     doctor.pop();
                 }
                 doctor.push(
-                    {id: '59fe476a533c0e24a08f8e67', fname: 'Lukas',sname: 'Simko', location:'Clonmel', contact: 35367544446, cost: 60}
+                    {id: '59f6f0b99bd0000f555a7dac', fname: 'Peter',sname: 'Korfanta', location:'Cork', contact: 353876278131, cost: 50}
                 );
 
             });
             chai.request(server)
-                .delete('/doctor/59fe476a533c0e24a08f8e67')
+                .delete('/doctor/59f6f0b99bd0000f555a7dac')
                 .end(function(err, res){
                     expect(res).to.have.status(200);
                     expect(res.body).to.be.a('array');
