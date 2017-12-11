650296f3b7c37c0c8d9aca5c678ef6365339a183
Lukas Simko <sompo74@gmail.com>
2017-12-11 20:58:37 +0000

    Patient page and controller

---
+++ public/javascripts/controllers/patientcontroller.js
@@ -0,0 +1,20 @@
+var app = angular.module('DonationWebApp');
+
+app.controller('patientController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+    // create a message to display in our view
+    $scope.message = 'Please Enter the patients details';
+
+    $scope.rgstPatient = function(regData){
+        console.log("form submitted");
+        $http.post( '/patients', regData).success(function(data) {
+            $scope.patients = data;
+            $location.path('/currentpatients')
+            console.log(data);
+        })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
+]);
