485f77df7e803ddf5d65b710c9f32f81b8802541
Lukas Simko <sompo74@gmail.com>
2017-12-12 01:38:35 +0000

    List and delete

--- public/javascripts/controllers/allPatientsController.js
+++ public/javascripts/controllers/allPatientsController.js
@@ -3,4 +3,38 @@ var app = angular.module('DonationWebApp');
 
 app.controller('allPatientsController', ['$scope',  '$http', function($scope,  $http) {
     // create a message to display in our view
-    $scope.message = 'List of registered Patients!';}]);
+    $scope.message = 'List of registered Patients!';
+
+
+
+    findAll();
+
+    function findAll() {
+        $http.get('/patients')
+            .success(function (data) {
+                $scope.patients = data;
+                console.log(data);
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    $scope.delete = function(id) {
+        if (confirm("Are you sure you want to delete this Patient?")) {
+            console.log('Deleting patient  : ' + id);
+            $http.delete('/patients/' + id)
+                .success(function(data) {
+                    console.log(data);
+                    findAll();
+                })
+                .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+        }
+    };
+
+
+
+
+}]);
